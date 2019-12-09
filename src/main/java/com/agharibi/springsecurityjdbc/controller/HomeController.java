0e0a1779dd3d25e4c48a5f3d498d2789b99f3546
Armen G <gharibi.armen@yahoo.com>
2019-12-08 16:20:08 -0800

    Added spring security configuration for user authenticatoin and authorization

--- src/main/java/com/agharibi/springsecurityjdbc/controller/HomeController.java
+++ src/main/java/com/agharibi/springsecurityjdbc/controller/HomeController.java
@@ -1,7 +1,32 @@
 package com.agharibi.springsecurityjdbc.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h2>Welcome to Spring Security JDBC Authentication!</h2>");
+    }
+
+    @GetMapping("/user")
+    public String userHome() {
+        String user = getUser();
+        return ("<h2>Welcome " + user + " Spring Security JDBC Authentication!</h2>");
+    }
+
+    @GetMapping("/admin")
+    public String adminHome() {
+        String admin = getUser();
+        return ("<h2>Welcome " + admin + " Spring Security JDBC Authentication!</h2>");
+    }
+
+    private String getUser() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getUsername();
+    }
 }
